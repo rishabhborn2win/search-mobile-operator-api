01e5ba70fb7eff1178e0e4d89c728fd375286e18
Rishabh Mishra <55771529+rishabhborn2win@users.noreply.github.com>
2021-05-09 13:22:43 +0530

    updated data

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require("express");
 var app = express();
-var getMobileCircle = require('./function');
+var getMobileCircle = require("./function");
 var cors = require("cors");
 
 //Init Middleware
 app.use(express.json({ extended: false }));
 app.use(cors());
 
-app.get('/api/getDetail/:phone', (req, res) => {
-    const phoneNumber = req.params.phone;
-    var response = getMobileCircle(phoneNumber);
-    res.status(200).json({ details : response});
-})
-
+app.get("/api/getDetail/:phone", (req, res) => {
+  const phoneNumber = req.params.phone;
+  var response = getMobileCircle(phoneNumber);
+  res.status(200).json({ details: response });
+});
 
 //Declaring the servers
 var port = process.env.PORT || 500;
